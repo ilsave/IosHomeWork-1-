c9ab44f6e7fdc741ca91973e01289864ff8b7e5a
Ilya Gushchin <Ilya.Gushchin@swtecnn.com>
2021-03-18 11:20:45 +0300

    test commit from Xcode

--- IosHomework1/ViewController.swift
+++ IosHomework1/ViewController.swift
@@ -19,10 +19,10 @@ class ViewController: UIViewController {
     
     @IBAction func celsiusEditChanged(_ sender: Any) {
         if (textFieldCelsius.hasText){
-            let kelvin: Double? = Double(textFieldCelsius.text!)! - 273.15
-            textFieldKelvin.text = String(format: "%.2f", kelvin!)
+            let kelvin: Double = Double(textFieldCelsius.text!) - 273.15
+            textFieldKelvin.text = String(format: "%.2f", kelvin)
             
-            let far: Double = (Double(textFieldCelsius.text!) ?? 0 - 273.15) * 9 / 2 + 32
+            let far: Double = (Double(textFieldCelsius.text!) - 273.15) * 9 / 2 + 32
                    textFieldFahrenheit.text = String(format: "%.2f", far)
         } else {
             textFieldFahrenheit.text = ""
@@ -33,11 +33,11 @@ class ViewController: UIViewController {
     
     @IBAction func kelvinEditChanged(_ sender: Any) {
         if (textFieldKelvin.hasText){
-            let cel: Int? = Int(textFieldKelvin.text!)! - 273
-            textFieldCelsius.text = String(cel!)
+            let cel: Int = Int(textFieldKelvin.text!) - 273
+            textFieldCelsius.text = String(cel)
             
             //(0 K − 273,15) × 9/5 + 32 = -459,7 °F
-            let far: Double = (Double(textFieldKelvin.text!) ?? 0 - 273.15) * 9 / 2 + 32
+            let far: Double = (Double(textFieldKelvin.text!) - 273.15) * 9 / 2 + 32
                    textFieldFahrenheit.text = String(far)
         } else {
             textFieldCelsius.text = ""
