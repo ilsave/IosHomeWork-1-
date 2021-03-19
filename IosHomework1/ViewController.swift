7caddc578effcf178c3ee4cc45e7a5f3f74c9f28
Ilya Gushchin <Ilya.Gushchin@swtecnn.com>
2021-03-19 11:28:31 +0300

    small refactoring

--- IosHomework1/ViewController.swift
+++ IosHomework1/ViewController.swift
@@ -13,31 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet var textFieldCelsius: UITextField!
     @IBOutlet var textFieldFahrenheit: UITextField!
     
+    let kelvinDifference: Double = 273.15
+    let fahrenheitCofficient: Double = 9 / 5
+    let fahrenheitCofficientReversed: Double = 5 / 9
+    let fahrenheitDifference: Double = 32
+    let twoNumbersLeft: Double = 100
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func celsiusEditChanged(_ sender: Any) {
-        if (textFieldCelsius.hasText){
-            let kelvin: Double = Double(textFieldCelsius.text!)! - 273.15
-            textFieldKelvin.text = String(format: "%.2f", kelvin)
-            
-            let far: Double = (Double(textFieldCelsius.text!)! - 273.15) * 9 / 2 + 32
-                   textFieldFahrenheit.text = String(format: "%.2f", far)
-        } else {
+        
+        guard textFieldCelsius.hasText else {
             textFieldFahrenheit.text = ""
             textFieldKelvin.text = ""
+            return
         }
+        
+        guard let celsiusText = textFieldCelsius.text else {
+            return
+        }
+    
+        
+        let kelvin: Double = Double(celsiusText)! - kelvinDifference
+        let kelvinRounded = Double(round(twoNumbersLeft*kelvin)/twoNumbersLeft)
+        textFieldKelvin.text = String(kelvinRounded)
+        
+        // textFieldKelvin.text = String(format: "%.2f", kelvin)
+        
+        let far: Double = (Double(celsiusText)! - kelvinDifference) * fahrenheitCofficient + fahrenheitDifference
+        textFieldFahrenheit.text = String(format: "%.2f", far)
     }
+    
+    
     
     
     @IBAction func kelvinEditChanged(_ sender: Any) {
         if (textFieldKelvin.hasText){
-            let cel: Int = Int(textFieldKelvin.text!)! - 273
+            let cel: Double = Double(textFieldKelvin.text!)! - kelvinDifference
             textFieldCelsius.text = String(cel)
             
             //(0 K − 273,15) × 9/5 + 32 = -459,7 °F
-            let far: Double = (Double(textFieldKelvin.text!)! - 273.15) * 9 / 2 + 32
+            let far: Double = (Double(textFieldKelvin.text!)! - kelvinDifference) * fahrenheitCofficient + fahrenheitDifference
                    textFieldFahrenheit.text = String(far)
         } else {
             textFieldCelsius.text = ""
@@ -49,37 +68,16 @@ class ViewController: UIViewController {
     @IBAction func fahrenheitEditChanged(_ sender: Any) {
         if (textFieldFahrenheit.hasText){
            // (32 °F − 32) × 5/9 = 0 °C
-            let cel: Double = (Double(textFieldFahrenheit.text!)! - 32) * 5 / 9
+            let cel: Double = (Double(textFieldFahrenheit.text!)! - fahrenheitDifference) * fahrenheitCofficientReversed
             textFieldCelsius.text = String(format: "%.2f", cel)
             
             //(32 °F − 32) × 5/9 + 273,15 = 273,15 K
-            let kelvin: Double = (Double(textFieldFahrenheit.text!)! - 32) * 5 / 9 + 273.15
+            let kelvin: Double = (Double(textFieldFahrenheit.text!)! - fahrenheitDifference) * fahrenheitCofficientReversed + kelvinDifference
             textFieldKelvin.text = String(format: "%.2f", kelvin)
         } else {
             textFieldCelsius.text = ""
             textFieldKelvin.text = ""
         }
     }
-    
-    
+}
 
-}
-extension UIViewController: UITextFieldDelegate{
-    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        let tapGesture = UITapGestureRecognizer(target: self,
-                         action: #selector(hideKeyboard))
-        view.addGestureRecognizer(tapGesture)
-        return true
-    }
-}
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tapGesture = UITapGestureRecognizer(target: self,
-                         action: #selector(hideKeyboard))
-        view.addGestureRecognizer(tapGesture)
-    }
-
-    @objc func hideKeyboard() {
-        view.endEditing(true)
-    }
-}
